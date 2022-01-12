6457f02aa9872627510432f09b96f055fb8385cd
joaoviss <joaoviss@gmail.com>
2022-01-12 20:43:55 -0300

    crud

--- src/main/java/com/joaoviss/api_jogos/controller/JogoController.java
+++ src/main/java/com/joaoviss/api_jogos/controller/JogoController.java
@@ -1,7 +1,6 @@
 package com.joaoviss.api_jogos.controller;
 
 import java.util.List;
-import java.util.Optional;
 import com.joaoviss.api_jogos.model.Jogo;
 import com.joaoviss.api_jogos.repository.JogoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,43 +8,57 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @CrossOrigin
 @RestController
+@RequestMapping("/jogos")
 public class JogoController {
     
     @Autowired
     private JogoRepository jogoRepository;
     
-    @RequestMapping(value="/jogos", method = RequestMethod.GET)
-    public List<Jogo> listAll() {
-        return jogoRepository.findAll();
-    }
-    
-    @RequestMapping(value = "/jogos/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Jogo> GetById(@PathVariable(value = "id") Long id) {
-        Optional<Jogo> jogo = jogoRepository.findById(id);
-        return (jogo.isPresent()) ?
-            new ResponseEntity<Jogo>(jogo.get(), HttpStatus.OK) :
-            new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-        
-    @RequestMapping(value = "/jogos", method = RequestMethod.POST)
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Jogo save(@RequestBody Jogo jogo) {
         return jogoRepository.save(jogo);
     }
         
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-    public ResponseEntity<String> delete(@PathVariable(value = "id") Long id) {
-        Optional<Jogo> jogo = jogoRepository.findById(id);
-        if (jogo.isPresent()) {
-            jogoRepository.deleteById(id);
-            return new ResponseEntity<String>("Jogo "+id+"deletado com sucesso!", HttpStatus.OK);
-        } else
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping
+    public List<Jogo> listAll() {
+        return jogoRepository.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Jogo> getById(@PathVariable(value = "id") Long id) {
+        return jogoRepository.findById(id)
+            .map (jogo -> ResponseEntity.ok(jogo))
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Jogo> update(@PathVariable Long id, @RequestBody Jogo jogo) {
+        if(!jogoRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        jogo.setId(id);
+        jogo = jogoRepository.save(jogo);
+        return ResponseEntity.ok(jogo);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable(value = "id") Long id) {
+        if(!jogoRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        jogoRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
